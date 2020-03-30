462b8177b46c08880e606315dba2a36e0b8d52dd
markm7777 <mark.murphy7777@gmail.com>
2020-03-30 07:13:32 -0700

    Updates

---
+++ precache-manifest.214bae755d64a784829c3cbf2ba49587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767e84d1e89fb3fe4eeb70eedb1fe9a3",
+    "url": "/WinnetkaPaintWorks/index.html"
+  },
+  {
+    "revision": "8a999c783bf2d0d2b38b",
+    "url": "/WinnetkaPaintWorks/static/css/main.a9b588af.chunk.css"
+  },
+  {
+    "revision": "c9cc21e9e5f52eedbe60",
+    "url": "/WinnetkaPaintWorks/static/js/2.07b935a9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/WinnetkaPaintWorks/static/js/2.07b935a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a999c783bf2d0d2b38b",
+    "url": "/WinnetkaPaintWorks/static/js/main.987e5817.chunk.js"
+  },
+  {
+    "revision": "0f13645c3ad1c1d94726",
+    "url": "/WinnetkaPaintWorks/static/js/runtime-main.ff73edbb.js"
+  }
+]);
